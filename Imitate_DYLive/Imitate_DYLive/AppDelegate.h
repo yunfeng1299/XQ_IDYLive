7099465a915ca1f8c8c7ea9a56dda8d50a613cee
夏琪 <18647530288@163.com>
2017-05-02 13:55:23 +0800

    begin

---
+++ Imitate_DYLive/Imitate_DYLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imitate_DYLive
+//
+//  Created by 夏琪 on 2017/5/2.
+//  Copyright © 2017年 yunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imitate_DYLive/Imitate_DYLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imitate_DYLive
+//
+//  Created by 夏琪 on 2017/5/2.
+//  Copyright © 2017年 yunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
